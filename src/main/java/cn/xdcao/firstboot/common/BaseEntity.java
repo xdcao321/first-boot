6f68bb38034bfead4b079edbce12d2f97a38d2e5
xdcao321 <king.xiao@foxmal.com>
2020-03-13 08:52:53 +0800

    add entity

---
+++ src/main/java/cn/xdcao/firstboot/common/BaseEntity.java
@@ -0,0 +1,16 @@
+package cn.xdcao.firstboot.common;
+
+import java.io.Serializable;
+
+/**
+ * @author xd.cao
+ * @version 1.0.0
+ * @ClassName: BaseEntity.java
+ * @Description: base entity
+ * @date 2020年03月13日 01:36:00
+ */
+public class BaseEntity implements Serializable {
+    private Long objectId;
+    private String owner;
+
+}
---
+++ src/main/java/cn/xdcao/firstboot/domain/DomainConstants.java
@@ -0,0 +1,13 @@
+package cn.xdcao.firstboot.domain;
+
+/**
+ * @author xd.cao
+ * @version 1.0.0
+ * @ClassName: DomainConstants.java
+ * @Description: Common String
+ * @date 2020年03月13日 02:12:00
+ */
+public interface DomainConstants {
+    String SELECT_ID = "ObjectId";
+    String STATE_INACTIVE = "inactive";
+}
